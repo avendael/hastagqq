1735d941cccaf22c3ddca7acdfb7085bf901339b
Evan Dale Aromin <avendael@gmail.com>
2013-08-17 17:49:16 +0800

    Changed tags to categories. News querying.

--- tornado/api.py
+++ tornado/api.py
@@ -1,4 +1,4 @@
-import time
+import datetime
 import json
 
 from tornado import ioloop
@@ -7,27 +7,27 @@ from dal import ScopedSession
 from models import Device, News
 from sqlalchemy.orm.exc import NoResultFound
 
-SUCCESS = '00000'
-FAIL = '00001'
+SUCCESS = "00000"
+FAIL = "00001"
 
 
 class DeviceRegistrationHandler(web.RequestHandler):
     def post(self):
         db = self.application.db
-        data = json.loads(self.request.body.decode('utf8'))
+        data = json.loads(self.request.body.decode("utf8"))
 
         try:
             device = db.query(Device).filter(
-                Device.gcm_id == data['gcm_id']).one()
+                Device.gcm_id == data["gcm_id"]).one()
             response = {
-                'message': 'You are already registered',
-                'response': FAIL}
+                "message": "You are already registered",
+                "response": FAIL}
             self.write(json.dumps(response))
         except NoResultFound:
-            device = Device(gcm_id=['gcm_id'])
+            device = Device(gcm_id=data["gcm_id"])
             response = {
-                'message': 'Successfully registered',
-                'response': FAIL
+                "message": "Successfully registered",
+                "response": FAIL
             }
 
             db.add(device)
@@ -38,19 +38,19 @@ class DeviceRegistrationHandler(web.RequestHandler):
 
 
 class NewsHandler(web.RequestHandler):
-    def post(self):
+    def post(self, query_params=None):
         db = self.application.db
-        data = json.loads(self.request.body.decode('utf8'))
+        data = json.loads(self.request.body.decode("utf8"))
         news = News(
-            title=data['title'],
-            content=data['content'],
-            location=data['location'],
-            tag=data['tag'],
-            timestamp=time.stftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+            title=data["title"],
+            content=data["content"],
+            location=data["location"],
+            category=data["category"],
+            timestamp=datetime.datetime.today()
         )
         response = {
-            'message': 'Successfully created news',
-            'response': SUCCESS
+            "message": "Successfully created news",
+            "response": SUCCESS
         }
 
         db.add(news)
@@ -59,16 +59,66 @@ class NewsHandler(web.RequestHandler):
 
         self.write(json.dumps(response))
 
-    def get(self):
+    def get(self, query_params=None):
         db = self.application.db
-        data = json.loads(self.request.body.decode('utf8'))
-        location = data['location']
-        news_items = db.query(News).filter(News.location == location)
         response = {
-            'news_items': news_items,
-            'message': 'Successfully retrieved news',
-            'response': SUCCESS
+            "response": FAIL,
+            "message": "Invalid query"
         }
+
+        if query_params is None or query_params == "":
+            location = self.request.headers["device_location"]\
+                       if "device_location" in self.request.headers else None
+
+            if location is None or location == "":
+                self.write(json.dumps(response))
+            else:
+                news_items = db.query(News).filter(News.location == location)
+                json_news_items = [{
+                    "title": news.title,
+                    "content": news.content,
+                    "location": news.location,
+                    "category": news.category,
+                    "score": news.score
+                } for news in news_items]
+                response = {
+                    "news_items": json_news_items,
+                    "message": "Successfully retrieved news",
+                    "response": SUCCESS
+                }
+        else:
+            try:
+                params = query_params.split("&")
+
+                news = None
+                for param in params:
+                    arg = param.split("=")
+                    if len(params) < 2 or len(params) > 2:
+                        self.write(json.dumps(response))
+                    else:
+                        if news is None:
+                            news = db.query(News).filter(
+                                getattr(News, arg[0]) == arg[1])
+                        else:
+                            news.filter(getattr(News, arg[0] == arg[1]))
+                        response = {
+                            "response": SUCCESS,
+                            "message": "Retrieved specific news item",
+                            "news": {
+                                "title": news.title,
+                                "content": news.content,
+                                "location": news.location,
+                                "category": news.category,
+                                "score": news.score
+                            }
+                        }
+            except NoResultFound:
+                response = {
+                    "message": "No such item",
+                    "response": FAIL
+                }
+            except AttributeError:
+                self.write(json.dumps(response))
 
         self.write(json.dumps(response))
 
@@ -76,7 +126,8 @@ class NewsHandler(web.RequestHandler):
 class WebApi(web.Application):
     def __init__(self):
         handlers = [
-            (r'/', DeviceRegistrationHandler),
+            (r"/device/?", DeviceRegistrationHandler),
+            (r'/news/?([a-zA-Z0-9=&"]*)/?', NewsHandler),
         ]
 
         web.Application.__init__(self, handlers)
--- tornado/models.py
+++ tornado/models.py
@@ -1,14 +1,27 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import Table
-from sqlalchemy.orm import relationship
-
 
 Base = declarative_base()
-news_tags = Table(
-    'news_tags', Base.metadata,
-    Column('news_id', Integer, ForeignKey('news.id', ondelete='cascade')),
-    Column('tag_id', Integer, ForeignKey('tags.id', ondelete='cascade')))
+
+
+class Device(Base):
+    __tablename__ = 'devices'
+    id = Column(Integer, primary_key=True)
+    gcm_id = Column(String(1024), nullable=False, unique=True, index=True)
+    location = Column(String())
+
+    def __repr__(self):
+        return "<Device: %s>" % self.gcm_id
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+
+    def __repr__(self):
+        return "<Category: %s>" % self.name
 
 
 class News(Base):
@@ -19,28 +32,8 @@ class News(Base):
     content = Column(String())
     location = Column(String())
     timestamp = Column(DateTime, nullable=False)
-    tags = relationship("Tag", secondary=news_tags,
-                               backref="news_articles",
-                               cascade="all, delete")
+    category = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    score = Column(Integer)
 
     def __repr__(self):
-        return "<News: %s -- %s>" % (self.title, self.content)
-
-
-class Tag(Base):
-    __tablename__ = 'tags'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String())
-
-    def __repr__(self):
-        return "<Tag: %s>" % self.name
-
-
-class Device(Base):
-    __tablename__ = 'devices'
-    id = Column(Integer, primary_key=True)
-    gcm_id = Column(String(1024), nullable=False, unique=True, index=True)
-
-    def __repr__(self):
-        return "<Device: %s>" % self.gcm_id
+        return "<News: %s -- %s>" % (self.title, self.category.name)
