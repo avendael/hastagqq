18f4d4c2e1d099a2733a47e2cd55f36e99242fbd
Evan Dale Aromin <avendael@gmail.com>
2013-08-18 09:54:15 +0800

    Standard GCM boilerplate.

--- android/app/src/com/hastagqq/app/GCMBroadcastReceiver.java
+++ android/app/src/com/hastagqq/app/GCMBroadcastReceiver.java
@@ -1,15 +1,26 @@
 package com.hastagqq.app;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
 /**
  * @author avendael
  */
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    private static final String TAG = GcmBroadcastReceiver.class.getSimpleName();
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "::onReceive() -- START");
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
 
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+        Log.d(TAG, "::onReceive() -- END");
     }
 }
